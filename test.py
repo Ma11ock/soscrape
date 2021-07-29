588daf1f74c12dcde7de2cbad984004a7b733ca3
Ryan Jeffrey <ryan@ryanmj.xyz>
2021-07-29 13:38:40 -0700

    Basic flash app.

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ import requests
 import sys
 import urllib
 import json
+from flask import Flask, request, jsonify
+#from flask_restful import Resource, api
 
 res = requests.get('https://stackoverflow.com/questions/tagged/'+''.join(sys.argv[1:]))
 
@@ -14,6 +16,8 @@ questions = soup.select(".question-summary")
 questions_data = {
     "questions": []
 }
+
+app = Flask(__name__)
 
 def get_question_description(http_page):
     desc_soup = BeautifulSoup(http_page, "lxml")
@@ -40,39 +44,56 @@ def get_answer(http_page):
             all_text += res + '\n'
     return all_text
 
-i = 0
-for que in questions:
-    if i >= 5:
-        break
-    unanswered = que.select_one('.unanswered')
-    if(unanswered):
-        continue
-    link = que.select_one('.question-hyperlink')
-    q = link.getText()
-    a = "https://stackoverflow.com" + link.get('href')
-    vote_count = que.select_one('.vote-count-post').getText()
-    views = que.select_one('.views').attrs['title']
-    answer = ""
-    qdesc = ""
 
-    try:
-        desc_html = requests.get(a)
-        desc_html.raise_for_status()
-        qdesc = get_question_description(desc_html.text)
-        answer = get_answer(desc_html.text)
-    except requests.exceptions.HTTPError:
-        print("The URL {} is invalid!".format(a), file=sys.stderr)
-        continue
-    questions_data['questions'].append({
-        "question_title": q,
-        "question_description": qdesc,
-        "views": views,
-        "vote_count": vote_count,
-        "link": a,
-        "answer": answer
-    })
-    i += 1
+def setup_app():
+    print("Setting up the app!")
+    i = 0
+    for que in questions:
+        if i >= 5:
+            break
+        unanswered = que.select_one('.unanswered')
+        if(unanswered):
+            continue
+        link = que.select_one('.question-hyperlink')
+        q = link.getText()
+        a = "https://stackoverflow.com" + link.get('href')
+        vote_count = que.select_one('.vote-count-post').getText()
+        views = que.select_one('.views').attrs['title']
+        answer = ""
+        qdesc = ""
 
-# json_data = json.dumps(questions_data)
+        try:
+            desc_html = requests.get(a)
+            desc_html.raise_for_status()
+            qdesc = get_question_description(desc_html.text)
+            answer = get_answer(desc_html.text)
+        except requests.exceptions.HTTPError:
+            print("The URL {} is invalid!".format(a), file=sys.stderr)
+            continue
+        questions_data['questions'].append({
+            "question_title": q,
+            "question_description": qdesc,
+            "views": views,
+            "vote_count": vote_count,
+            "link": a,
+            "answer": answer
+        })
+        i += 1
+
 
 print(questions_data)
+
+
+@app.get("/questions")
+def get_questions():
+    return jsonify(questions_data)
+
+
+@app.get("/question")
+def get_question():
+    return jsonify(questions_data["questions"][0])
+
+
+if __name__ == "__main__":
+    setup_app()
+    app.run()
