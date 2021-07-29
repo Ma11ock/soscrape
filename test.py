7868c92638b021d8647cd817d6bf07d5d9fa085b
Ryan Jeffrey <ryan@ryanmj.xyz>
2021-07-29 12:46:46 -0700

    get answer

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, NavigableString, Tag
 import requests
 import sys
 import urllib
@@ -15,20 +15,64 @@ questions_data = {
     "questions": []
 }
 
+def get_question_description(http_page):
+    desc_soup = BeautifulSoup(http_page, "lxml")
+    desc = desc_soup.find("div", {"class": "js-post-body"})
+    all_text = ""
+    for res in desc:
+        if isinstance(res, Tag):
+            all_text += res.text + '\n'
+        elif isinstance(res, NavigableString):
+            all_text += res + '\n'
+
+
+    return all_text
+
+def get_answer(http_page):
+    answer_soup = BeautifulSoup(http_page, "lxml")
+    answer_cell = answer_soup.find("div", {"id": "answers"})
+    answer = answer_cell.find("div", {"class": "js-post-body"})
+    all_text = ""
+    for res in answer:
+        if isinstance(res, Tag):
+            all_text += res.text + '\n'
+        elif isinstance(res, NavigableString):
+            all_text += res + '\n'
+    return all_text
+
+i = 0
 for que in questions:
+    if i >= 5:
+        break
     unanswered = que.select_one('.unanswered')
     if(unanswered):
         continue
     link = que.select_one('.question-hyperlink')
     q = link.getText()
-    a = link.get('href')
+    a = "https://stackoverflow.com" + link.get('href')
     vote_count = que.select_one('.vote-count-post').getText()
     views = que.select_one('.views').attrs['title']
+    answer = ""
+    qdesc = ""
+
+    try:
+        desc_html = requests.get(a)
+        desc_html.raise_for_status()
+        qdesc = get_question_description(desc_html.text)
+        answer = get_answer(desc_html.text)
+    except requests.exceptions.HTTPError:
+        print("The URL {} is invalid!".format(a), file=sys.stderr)
+        continue
     questions_data['questions'].append({
-        "questions": q,
+        "question_title": q,
+        "question_description": qdesc,
         "views": views,
         "vote_count": vote_count,
-        "link": "https://stackoverflow.com" + a,
+        "link": a,
+        "answer": answer
     })
+    i += 1
+
+# json_data = json.dumps(questions_data)
 
 print(questions_data)
